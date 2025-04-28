7f8011dd0eed6a5b30e47fea342e31fc6f9a3bd2
Stefan Bejenescu <sbejenescubabusanu@gmail.com>
2025-04-28 17:18:08 +0300

    lab-08: to-upper: Added solution to exercise
    Signed-off-by: Stefan Bejenescu <sbejenescubabusanu@gmail.com>

--- labs/lab-08/tasks/to-upper/support/to_upper.asm
+++ labs/lab-08/tasks/to-upper/support/to_upper.asm
@@ -8,7 +8,18 @@ to_upper:
     push ebx                ; preserve ebx as required by cdecl
 
     ; TODO
+    mov eax, [ebp + 8]
 
+check_byte:
+    mov bl, [eax]
+    test bl, bl
+    je out
+    sub bl, 0x20
+    mov [eax], bl
+    inc eax
+    jmp check_byte
+
+out:
     pop ebx
     leave
     ret
